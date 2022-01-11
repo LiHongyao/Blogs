e1a58b4b59d996654489a797b08b07dda6662665
Li-HONGYAO <lihy_online@163.com>
2022-01-11 19:58:27 +0800

    内容更新

---
+++ CODES/ast/app.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: Lee
+ * @Date: 2022-01-11 17:53:20
+ * @LastEditors: Lee
+ * @LastEditTime: 2022-01-11 17:53:21
+ */
+[
+  { type: 'Keyword', value: 'const' },
+  { type: 'Identifier', value: 'a' },
+  { type: 'Punctuator', value: '=' },
+  { type: 'Numeric', value: '10' },
+  { type: 'Punctuator', value: ';' },
+];
