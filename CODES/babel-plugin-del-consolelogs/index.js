56ef6bb8235bfc8eb8b4d11c49bb383a0f3e9b7d
lihongyao <lihy_online@163.com>
2022-01-13 02:41:18 +0800

    内容更新

--- CODES/babel-plugin-del-consolelogs/index.js
+++ CODES/babel-plugin-del-consolelogs/index.js
@@ -2,19 +2,29 @@
  * @Author: Lee
  * @Date: 2022-01-12 20:29:14
  * @LastEditors: Lee
- * @LastEditTime: 2022-01-12 21:11:00
+ * @LastEditTime: 2022-01-13 02:40:29
  */
+
 module.exports = ({ types: t }) => {
   return {
+    /** 插件名称 */
+    name: 'babel-plugin-del-consolelogs',
+    /** 访问者 */
     visitor: {
-      // coding in here...
       CallExpression(path) {
-        const hasCallee = !!path.node.callee;
-        const isConsole = t.isIdentifier(path.node.callee.object, {
+        const parentPath = path.parentPath;
+        // → 筛选是否是打印语句
+        const isConsoleStatement = t.isIdentifier(path.node.callee.object, {
           name: 'console',
         });
-        if (hasCallee && isConsole) {
-          path.remove();
+        if (isConsoleStatement) {
+          // → 判断是否需要移除注释，即代码中如果存在 @lg-noremove 则不清除
+          // → 分两种情况：前置注释 和 后置注释
+          let shouldRemove = true;
+          const a = parentPath.get("@lg-noremove");
+          console.log(a., '------');
+         
+          // path.remove();
         }
       },
     },
--- CODES/babel-plugin-del-consolelogs/test.js
+++ CODES/babel-plugin-del-consolelogs/test.js
@@ -2,21 +2,29 @@
  * @Author: Lee
  * @Date: 2022-01-12 20:32:15
  * @LastEditors: Lee
- * @LastEditTime: 2022-01-12 20:37:25
+ * @LastEditTime: 2022-01-13 02:20:25
  */
+// → 导入 transformSync
 const { transformSync } = require('@babel/core');
-// → 使用我们的插件
+
+// → 编写示例代码
 
 const code = `
   const a = 10;
   const b = 20;
   console.log(a);
+  // hello
+  // @noremove
   console.log(b);
-  console.log('Hello, vue.js!');
+  console.log('Hello, vue.js!'); //@noremove
+  console.log('happy');
   const sum = a + b;
 `;
 
+// → 调用插件处理code
 const output = transformSync(code, {
   plugins: ['./index.js'],
 });
+
+// → 输出目标代码
 console.log(output.code);
