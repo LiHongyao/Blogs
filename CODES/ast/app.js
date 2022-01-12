4777eb6b6b9240dcb3703fa8559e6469e895c83b
Li-HONGYAO <lihy_online@163.com>
2022-01-12 18:53:55 +0800

    内容更新

--- CODES/ast/app.js
+++ CODES/ast/app.js
@@ -1,13 +1,30 @@
 /*
  * @Author: Lee
- * @Date: 2022-01-11 17:53:20
+ * @Date: 2022-01-12 14:12:05
  * @LastEditors: Lee
- * @LastEditTime: 2022-01-11 17:53:21
+ * @LastEditTime: 2022-01-12 18:17:59
  */
-[
-  { type: 'Keyword', value: 'const' },
-  { type: 'Identifier', value: 'a' },
-  { type: 'Punctuator', value: '=' },
-  { type: 'Numeric', value: '10' },
-  { type: 'Punctuator', value: ';' },
-];
+
+// → 导入模块
+const babelParser = require('@babel/parser');
+
+// → 定义一段脚本
+const codeStr = `function square(n) {
+    return n * n;
+  }`;
+
+// → 通过解析器脚本生成AST
+const ast = babelParser.parse(codeStr);
+
+// → 递归遍历AST，进行节点操作
+require('@babel/traverse').default(ast, {
+  enter(path) {
+    if (path.isIdentifier({ name: 'n' })) {
+      path.node.name = 'x';
+    }
+  },
+});
+
+require('@babel/generator').default(ast).code;
+// → 打印AST
+console.log();
